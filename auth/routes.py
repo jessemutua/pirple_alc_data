ab004b3c383202d3b9511becb1ffd83f5136c95a
Jesse Mutua <jessemutua76@gmail.com>
2026-03-17 15:25:43 +0300

    And there was light

--- auth/routes.py
+++ auth/routes.py
@@ -11,10 +11,8 @@ def serialize_user(user: User):
     return {
         "id": user.id,
         "email": user.email,
-        "created_at": user.created_at.isoformat(),
+        "created_at": user.created_at.isoformat() if user.created_at else None,
     }
-
-
 @router.post("/register", response_model=AuthResponse)
 def register(payload: AuthPayload):
     db = SessionLocal()

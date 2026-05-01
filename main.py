f83144840ca9fde06d8d1f89e15dd38a1f1b59c2
unknown <jessemutua76@gmail.com>
2026-05-01 12:41:48 +0300

    only way out is through

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import drinks.sober_routes as sober_routes
 from core.config import DATABASE_URL
 from core.database import init_db
 import analytics.refresh_routes as refresh_routes
-app.include_router(refresh_routes.router)
 
 print("✅ MAIN LOADED")
 
@@ -35,6 +34,7 @@ app.include_router(sessions_routes.router)
 app.include_router(calendar_routes.router)
 app.include_router(analytics_routes.router)
 app.include_router(sober_routes.router)
+app.include_router(refresh_routes.router)  # moved here
 
 @app.on_event("startup")
 def startup():
